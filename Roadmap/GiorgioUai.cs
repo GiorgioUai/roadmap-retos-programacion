6d9c1391135f2a3f3c3ea6541298e31418aa363d
GiorgioUai <132209009+GiorgioUai@users.noreply.github.com>
2024-05-12 14:11:26 -0300

    GiorgioUai.cs
    mi resolución al Reto #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO

---
+++ Roadmap/GiorgioUai.cs
@@ -0,0 +1,30 @@
+//https://dotnet.microsoft.com/es-es/languages/csharp
+
+//comentar una linea
+
+/*
+ * aca se puede comentar
+ * en varias lineas
+*/
+
+//tambien se puede selecionar varias lineas y usar Ctrl+E,C
+//    para comentar varias lineas y 
+//    se utiliza Ctrl+E,U
+//para desseleccionar 
+
+int numero_de_32_bits = 1245764564;
+double numero2 = 1515164646.516565115616;
+float numero3 = 6165151.5551F;
+char caracter = 'a';
+decimal numero4 = 0.15415151M;
+string texto = "aca se puede escribir texto";
+bool variable_logica1 = false;
+bool variable_logica2 = true;
+short numero_de_16_bits = 0;
+long numero_de_64_bits = 1345345345435435435;
+ushort numero_entero_sin_signo_de_16_bits = 6;
+
+const int NUMERO = 12;
+const double NUMERO2 = 10.15;
+
+Console.WriteLine("¡Hola, C#!");
